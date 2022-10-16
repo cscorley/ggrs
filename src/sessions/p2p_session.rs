a7bccb6b84addb1e2e2312bb13e36d788a5c1477
Christopher Corley <cscorley@gmail.com>
2022-10-16 10:33:09 -0400

    only send if endpoint is running

--- src/sessions/p2p_session.rs
+++ src/sessions/p2p_session.rs
@@ -518,10 +518,14 @@ impl<T: Config> P2PSession<T> {
     /// Additionally, spectators can only receive data from the host
     pub fn send_user_data(&mut self, data: Vec<u8>) {
         for endpoint in self.player_reg.remotes.values_mut() {
-            endpoint.send_user_data(data.clone());
+            if endpoint.is_running() {
+                endpoint.send_user_data(data.clone());
+            }
         }
         for endpoint in self.player_reg.spectators.values_mut() {
-            endpoint.send_user_data(data.clone());
+            if endpoint.is_running() {
+                endpoint.send_user_data(data.clone());
+            }
         }
     }
 
